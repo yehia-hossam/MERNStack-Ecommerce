02583d5f15698c37bbc569c97c86ce93e2a69635
yehia-hossam <yehiahossamdev@gmail.com>
2025-04-14 14:36:51 +0200

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,28 @@
-const express = require('express')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-require('dotenv').config()
-const connectDB = require('./config/db')
-const router = require('./routes')
+require('dotenv').config();
 
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
+const router = require('./routes');  
 
-const app = express()
+const app = express();
+
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
-    credentials : true
-}))
-app.use(express.json())
-app.use(cookieParser())
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
 
-app.use("/api",router)
+app.use(express.json());
+app.use(cookieParser());
 
-const PORT = 8080 || process.env.PORT
+app.use("/api", router);  
 
+const PORT = process.env.PORT || 8080;
 
-connectDB().then(()=>{
-    app.listen(PORT,()=>{
-        console.log("connnect to DB")
-        console.log("Server is running "+PORT)
-    })
-})
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("connected to DB");
+        console.log("Server is running on port " + PORT);
+    });
+});
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='bg-slate-200'>
-      <div className='container mx-auto p-4'>
-       <p className='text-center font-bold' title="Youtube Channel">Dynamic Coding with Amit</p>
+      <div className='container p-4 mx-auto'>
+       <p className='font-bold text-center' title="Youtube Channel">Dynamic Coding with Yehia Hossam</p>
       </div>
     </footer>
   )
